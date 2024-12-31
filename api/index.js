fa19135cd1f4e1e727158ac5ef9e8b9a2f461cb6
LeizaneMelo <60352187+LeizaneMelo@users.noreply.github.com>
2024-12-31 00:48:45 +0000

    porta

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@
 const express = require('express');
  const fs = require('fs');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 const cors = require('cors');
 
 // Habilitar CORS
